ac00787bc63dbf1fbb311aa8de1a7c043a7c444f
SergiiPodlishchuk <podluy23@gmail.com>
2020-10-27 22:55:04 +0200

    init node

---
+++ contacts.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import path from "path";
+
+const contactsPath = "path";
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
