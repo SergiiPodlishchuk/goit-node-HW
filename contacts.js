a63ea87ad030264fe22d62ec18bcfa257379d293
SergiiPodlishchuk <podluy23@gmail.com>
2020-11-01 19:04:40 +0200

    hw01_node

--- contacts.js
+++ contacts.js
@@ -1,23 +1,63 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
-const contactsPath = "path";
+const contactsPath = path.join(__dirname, "/db/contacts.json");
+
+const users = JSON.parse(fs.readFileSync(contactsPath).toString());
 
 // TODO: задокументировать каждую функцию
 function listContacts() {
-  // ...твой код
+  /**
+   * @param {contactsPath:}
+   */
+  console.table(
+    fs.readFile(contactsPath, "utf8", (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    })
+  );
 }
 
 function getContactById(contactId) {
-  // ...твой код
+  /**
+   * @param {contactId: number}
+   */
+  const userById = users.find((user) => user.id === contactId);
+  console.log(userById);
 }
 
 function removeContact(contactId) {
-  // ...твой код
+  /**
+   * @param {contactId: number}
+   */
+  const userById = users.find((user) => user.id === contactId);
+  console.log(userById);
+  if (!userById) {
+    console.log("No have user with this id");
+    return;
+  }
+
+  const indexUser = users.indexOf(userById);
+  users.splice(indexUser, 1);
+  const content = JSON.stringify(users, null, 1);
+
+  fs.writeFile(contactsPath, content, (err, data) => {});
+  console.log(users);
 }
 
 function addContact(name, email, phone) {
-  // ...твой код
+  /**
+   * @param {{name: string, email: string, phone: string}}
+   */
+  let maxId = 0;
+  const mapid = users.map((user) => {
+    user.id > maxId ? (maxId = user.id) : (maxId = 0);
+  });
+  const newUser = { id: maxId + 1, name, email, phone };
+  users.push(newUser);
+  const content = JSON.stringify(users, null, 1);
+  fs.writeFile(contactsPath, content, (err, data) => {});
+  console.log(users);
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const contacts = require("./contacts.js");
+const yargs = require("yargs");
 
 // index.js
 const argv = require("yargs").argv;
+console.log(argv);
 
 // TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      contacts.listContacts();
       break;
 
     case "get":
-      // ... id
+      contacts.getContactById(id);
       break;
 
     case "add":
       // ... name email phone
+      contacts.addContact(name, email, phone);
       break;
 
     case "remove":
       // ... id
+      contacts.removeContact(id);
       break;
 
     default:
