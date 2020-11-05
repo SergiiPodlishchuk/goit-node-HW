025edc314a045f92fa1a7d81f46dba6f91ee8f2f
SergiiPodlishchuk <podluy23@gmail.com>
2020-11-05 23:59:09 +0200

    ffk

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (request, response) => {
+app.get("/api/contacts", (request, response) => {
   response.send("Hello from Express!");
 });
+
+app.get("/api/contacts/:contactId", (req, res) => {});
+
 app.listen(port, (err) => {
   if (err) {
     return console.log("something bad happened", err);
